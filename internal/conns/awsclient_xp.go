5d704d4752cd19adb0b7fcc0a853678865c2e34a
Matt Bush <mbbush@gmail.com>
2024-04-22 15:48:35 -0700

    Expose AWSClient.Session publicly
    Signed-off-by: Matt Bush <mbbush@gmail.com>

--- internal/conns/awsclient_xp.go
+++ internal/conns/awsclient_xp.go
@@ -3,8 +3,14 @@
 
 package conns
 
-import "github.com/aws/smithy-go/middleware"
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/smithy-go/middleware"
+)
 
 func (c *AWSClient) AppendAPIOptions(options ...func(stack *middleware.Stack) error) {
 	c.awsConfig.APIOptions = append(c.awsConfig.APIOptions, options...)
+}
+func (c *AWSClient) Session() *session.Session {
+	return c.session
 }
